c3734bc758ad080306a6ae7f9338ea27faaa87b8
Daria Sova <todariasova@gmail.com>
2016-03-21 11:12:17 -0700

    Add FileSystem structs

---
+++ fat.h
@@ -0,0 +1,17 @@
+struct Superblock {
+  uint64_t system_id;
+  uint16_t block_size;
+  uint32_t blocks_num;
+  struct FAT *FAT;
+  struct FDirectory *FD;
+};
+
+struct FAT {
+  uint32_t first_block;
+  uint32_t blocks_num;
+};
+
+struct FDirectory {
+  uint32_t first_block;
+  uint32_t blocks_num;
+};
