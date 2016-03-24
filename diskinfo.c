e9d8a0105254fa5ea0ec8faf51b1e0a195027487
Daria Sova <todariasova@gmail.com>
2016-03-23 19:06:41 -0700

    finish part two

--- diskinfo.c
+++ diskinfo.c
@@ -148,47 +148,45 @@ int main ( int argc, char *argv[] )
       uint8_t* mask = (uint8_t*)&buf[pointer]; 
       pointer++;
 
-      switch(*mask&DIRECTORY_ENTRY_FILE)
+      if((*mask&DIRECTORY_ENTRY_USED)==DIRECTORY_ENTRY_USED)
       {
-        case DIRECTORY_ENTRY_FILE:
-          //skip first block info
-          pointer+=4;
-          pointer+=4;
-          uint32_t* file_size = (uint32_t*)&buf[pointer];
-          pointer+=4;
-          //skip creation date
-          pointer+=7;
-          uint16_t* year = (uint16_t*)&buf[pointer]; 
-          pointer+=2;
-          uint8_t* month = (uint8_t*)&buf[pointer]; 
-          pointer++;
-          uint8_t* day = (uint8_t*)&buf[pointer]; 
-          pointer++;
-          uint8_t* hour = (uint8_t*)&buf[pointer]; 
-          pointer++;
-          uint8_t* minute = (uint8_t*)&buf[pointer]; 
-          pointer++;
-          uint8_t* seconds = (uint8_t*)&buf[pointer]; 
-          pointer++;
-          //read file name
-          unsigned char file_name [30];
-          for(int k=0; k<31; k++)
-            file_name[k] = buf[pointer++];
-          pointer+=6;
-          //printf("\nPOINTER: %d\n", pointer);
-          printf("F\t", *mask);
-          printf("FileSize: %d\t", ntohl(*file_size));
-          printf("File name: %s\t", file_name);
-          printf("%d/%02X/%02X %i:%i:%X\n", htons(*year), *month, *day, *hour, *minute, *seconds);
-          break;
-        case DIRECTORY_ENTRY_DIRECTORY:
-          printf("DDDD");
-          break;
-        default:
-          break;
+        if((*mask&DIRECTORY_ENTRY_FILE)==DIRECTORY_ENTRY_FILE)
+        {
+          printf("F ");
+        }
+        else if((*mask&DIRECTORY_ENTRY_DIRECTORY)==DIRECTORY_ENTRY_DIRECTORY)
+        {
+          printf("D ");
+        }
+        //skip first block info
+        pointer+=4;
+        pointer+=4;
+        uint32_t* file_size = (uint32_t*)&buf[pointer];
+        pointer+=4;
+        //skip creation date
+        pointer+=7;
+        uint16_t* year = (uint16_t*)&buf[pointer]; 
+        pointer+=2;
+        uint8_t* month = (uint8_t*)&buf[pointer]; 
+        pointer++;
+        uint8_t* day = (uint8_t*)&buf[pointer]; 
+        pointer++;
+        uint8_t* hour = (uint8_t*)&buf[pointer]; 
+        pointer++;
+        uint8_t* minute = (uint8_t*)&buf[pointer]; 
+        pointer++;
+        uint8_t* seconds = (uint8_t*)&buf[pointer]; 
+        pointer++;
+        //read file name
+        unsigned char file_name [30];
+        for(int k=0; k<31; k++)
+          file_name[k] = buf[pointer++];
+        pointer+=6;
+
+        printf("%10d ", ntohl(*file_size));
+        printf("%30s ", file_name);
+        printf("%d/%02X/%02X %i:%i:%02i\n", htons(*year), *month, *day, *hour, *minute, *seconds);
       }
-
-
     }
   }
 
