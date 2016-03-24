8516a057cd29488c584b65725f81bcafeee38c2e
Daria Sova <todariasova@gmail.com>
2016-03-23 23:58:58 -0700

    Use structs

--- diskinfo.c
+++ diskinfo.c
@@ -3,6 +3,7 @@
 #include <inttypes.h>
 #include <arpa/inet.h>
 #include "constants.h"
+#include "fat.h"
 
 void get_name(FILE *fp, unsigned char *name)
 {
@@ -73,6 +74,13 @@ int main ( int argc, char *argv[] )
 
   fp = fopen(disk_image, "rb");
 
+  struct Superblock *superblock;
+  superblock = (struct Superblock*)malloc(sizeof(superblock));
+  struct FAT *FAT;
+  FAT = (struct FAT*)malloc(sizeof(FAT));
+  struct FDirectory *root_directory;
+  root_directory = (struct FDirectory*)malloc(sizeof(root_directory));
+
   uint32_t size = 0;
   uint32_t blocks_num = 0;
   uint32_t fat_starts = 0;
@@ -81,18 +89,18 @@ int main ( int argc, char *argv[] )
   uint32_t root_dir_start = 0;
 
   printf("Super block information:\n");
-  size = get_block_size(fp);
-  printf("Block size: %d\n", size);
-  blocks_num = get_blocks_num(fp);
-  printf("Blocks number: %d\n", blocks_num);
-  fat_starts = get_FAT_start(fp);
-  printf("FAT starts: %d\n", fat_starts);
-  fat_blocks = get_FAT_blocks(fp);
-  printf("FAT blocks: %d\n", fat_blocks);
-  root_dir_start = get_rootdir_start(fp);
-  printf("Root Directory start: %d\n", root_dir_start);
-  root_dir_blocks = get_rootdir_blocks(fp);
-  printf("Root directory blocks: %d\n", root_dir_blocks);
+  superblock->block_size = get_block_size(fp);
+  printf("Block size: %d\n", superblock->block_size);
+  superblock->blocks_num = get_blocks_num(fp);
+  printf("Blocks number: %d\n", superblock->blocks_num);
+  FAT->first_block = get_FAT_start(fp);
+  printf("FAT starts: %d\n", FAT->first_block);
+  FAT->blocks_num = get_FAT_blocks(fp);
+  printf("FAT blocks: %d\n", FAT->blocks_num);
+  root_directory->first_block = get_rootdir_start(fp);
+  printf("Root Directory start: %d\n", root_directory->first_block);
+  root_directory->blocks_num = get_rootdir_blocks(fp);
+  printf("Root directory blocks: %d\n", root_directory->blocks_num);
 
   //READ FAT BLOCK
   unsigned char buf[DEFAULT_BLOCK_SIZE];
@@ -103,7 +111,7 @@ int main ( int argc, char *argv[] )
 
   printf("\nFAT information:\n");
   //for every entry in a FAT block
-  for(int i= fat_starts; i<= fat_blocks; i++)
+  for(int i= FAT->first_block; i<= FAT->blocks_num; i++)
   {
     fseek(fp, i*DEFAULT_BLOCK_SIZE, SEEK_SET);
     fread(buf, 512, 1, fp);
