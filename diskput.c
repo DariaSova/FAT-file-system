82724f6c6dbb39ba315837e58eaf90bd5de8ebdc
Daria Sova <todariasova@gmail.com>
2016-03-28 20:48:22 -0700

    write file info into dir

--- diskput.c
+++ diskput.c
@@ -6,12 +6,16 @@
 #include <unistd.h> 
 #include <fcntl.h> 
 #include <math.h>
+#include <time.h>
 
 struct node {
   int entry;
   struct node *next;
 };
 struct node *root;
+int dir_block=0;
+int dir_entry=0;
+int dir_first=0;
 
 double file_size(int file)
 {
@@ -43,7 +47,7 @@ void free_entries(FILE *fp, struct FAT *FAT)
       switch(result)
       {
         case FAT_FREE:
-          current->entry = pointer;
+          current->entry = pointer/4;
           //printf("FREE: %i  %d ", result, current->entry);
           current->next=next;
           current=next;
@@ -51,6 +55,107 @@ void free_entries(FILE *fp, struct FAT *FAT)
           break;
       }
       pointer+=FAT_ENTRY_SIZE;
+    }
+  }
+};
+
+void write_file_into_dir(FILE *fp, char *file_name, int blocks_num, int file_size)
+{
+  
+  long offset =(long)(((dir_block)*DEFAULT_BLOCK_SIZE)+(dir_entry*64));
+  printf("\nOFFSET: %ld\n", offset);
+  fseek(fp, offset, SEEK_SET);
+  //CONVERT?
+  uint8_t mask = 0x03;
+  fwrite(&mask, 1, 1, fp);
+  //skip first block info
+  uint32_t first_block = ntohl(root->entry);
+  fwrite(&first_block, 4, 1, fp);
+  uint32_t blocks = ntohl(blocks_num);
+  fwrite(&blocks, 4, 1, fp);
+  uint32_t size = ntohl(file_size);
+  fwrite(&size, 4, 1, fp);
+
+  time_t current_time;
+  current_time = time(0);
+  struct tm t_now = *localtime(&current_time);
+  uint16_t year = (uint16_t)(t_now.tm_year + 1900);
+  year = 2016;
+  year = ntohs(year);
+  fwrite(&year, 2, 1, fp);
+
+  printf("DATA: %d, %i", t_now.tm_year+1900, t_now.tm_mon);
+  
+  //creation time
+  uint8_t month = (uint8_t)(t_now.tm_mon+1);
+  fwrite(&month, 1, 1, fp);
+  uint8_t day = (uint8_t)(t_now.tm_mday);
+  fwrite(&day, 1, 1, fp);
+  uint8_t hour = (uint8_t)(t_now.tm_hour); 
+  fwrite(&hour, 1, 1, fp);
+  uint8_t minute = (uint8_t)(t_now.tm_min); 
+  fwrite(&minute, 1, 1, fp);
+  uint8_t seconds = (uint8_t)(t_now.tm_sec);
+  fwrite(&seconds, 1, 1, fp);
+
+  //modify time
+  fwrite(&year, 2, 1, fp);
+  fwrite(&month, 1, 1, fp);
+  fwrite(&day, 1, 1, fp);
+  fwrite(&hour, 1, 1, fp);
+  fwrite(&minute, 1, 1, fp);
+  fwrite(&seconds, 1, 1, fp);
+
+  int pointer = 27;
+
+  int k=0;
+  while(file_name[k]!='\0')
+  {
+    uint8_t c = (uint8_t)file_name[k];
+    fwrite(&c, 1, 1, fp);
+    pointer++;
+    k++;
+  }
+  uint8_t c = (uint8_t)'\0';
+  fwrite(&c, 1, 1, fp);
+  pointer++;
+
+  uint8_t unused = 0xFF;
+  for(int i=pointer; i<64;i++)
+    fwrite(&unused, 1, 1, fp);
+    
+};
+
+void find_dir_block(FILE *fp)
+{
+  root_directory = (struct FDirectory*)malloc(sizeof(root_directory));
+  root_directory->first_block = get_rootdir_start(fp);
+  dir_first =root_directory->first_block;
+  root_directory->blocks_num = get_rootdir_blocks(fp);
+  unsigned char buf[DEFAULT_BLOCK_SIZE];
+  int found =0;
+
+  for(int i=root_directory->first_block; i< root_directory->first_block+root_directory->blocks_num; i++)
+  {
+    if(found==1)
+      break;
+
+    fseek(fp, i*DEFAULT_BLOCK_SIZE, SEEK_SET);
+    fread(buf, 512, 1, fp);
+    int pointer = 0;
+
+    for(int j=0; j< 8; j++)
+    {
+      uint8_t* mask = (uint8_t*)&buf[pointer]; 
+
+      if((*mask&DIRECTORY_ENTRY_USED)!=DIRECTORY_ENTRY_USED)
+      {
+        dir_block = i;
+        dir_entry =j;
+        found =1;
+        printf("\nDIRECTORY: %d, %d", i, j);
+        break;
+      }
     }
   }
 };
@@ -86,13 +191,21 @@ int main ( int argc, char *argv[] )
   }
 
   disk_image = argv[1];
-  int file=open(argv[2], O_RDONLY);
+  char *file_name =argv[2];
+  int file=open(file_name, O_RDONLY);
 
   double f_size = file_size(file);
   printf("File Size: \t%f bytes\n", f_size);
-  long blocks_number =blocks_needed(f_size);
+  long blocks_number = blocks_needed(f_size);
   printf("File Blocks: \t%ld blocks needed\n", blocks_number);
   find_free_fat_entries(disk_image);
+  FILE *fp;
+  fp = fopen(disk_image, "rb");
+  find_dir_block(fp);
+  fclose(fp);
+  fp = fopen(disk_image, "r+");
+  write_file_into_dir(fp, file_name, blocks_number, f_size);
 
+  fclose(fp);
   close(file);
 };
